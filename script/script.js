138c02d3eddaad9d20f49685fa25d9190372cbb4
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-09-24 23:41:14 +0500

    a

---
+++ script/script.js
@@ -0,0 +1,60 @@
+// 1-misol
+// let son = Math.floor(prompt("Sonni kiriting:")); 
+
+// if (son > 0) {
+//   son = Number(son) + 1;  
+// } else {
+//   son = Number(son) - 2;  
+// }
+
+// console.log("Natija: " + son);
+
+
+// 2-misol
+// let son1 = prompt("Birinchi sonni kiriting:");
+// let son2 = prompt("Ikkinchi sonni kiriting:");
+// let son3 = prompt("Uchinchi sonni kiriting:");
+
+// let musbat = 0; 
+// let manfiy = 0; 
+
+// if (son1 > 0) {
+//   musbat++;
+// } else if (son1 < 0) {
+//   manfiy++;
+// }
+
+// if (son2 > 0) {
+//   musbat++;
+// } else if (son2 < 0) {
+//   manfiy++;
+// }
+
+// if (son3 > 0) {
+//   musbat++;
+// } else if (son3 < 0) {
+//   manfiy++;
+// }
+
+// console.log("Musbat sonlar soni: " + musbat);
+// console.log("Manfiy sonlar soni: " + manfiy);
+
+
+// 3-misol
+// let son1 = prompt('1chi sonni kiriting: ');
+// let son2 = prompt('2chi sonni  kiriting: ');
+// let son3 = prompt('3ch  sonni kiriting: ');
+
+// let bosh = 0;
+// son1 = parseInt(son1);
+
+// son2 = parseInt(son2);
+// son3 = parseInt(son3);
+
+
+// if (son1 > 0) bosh++;
+// if (son2 > 0) bosh++;
+// if (son3 > 0) bosh++;
+// console.log("Musbat sonlar:" + bosh);
+
+
