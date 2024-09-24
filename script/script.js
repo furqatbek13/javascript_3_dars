06ef8d41e26d6384d76274c88ff527b08f222449
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-09-24 23:52:01 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -41,20 +41,77 @@
 
 
 // 3-misol
-// let son1 = prompt('1chi sonni kiriting: ');
-// let son2 = prompt('2chi sonni  kiriting: ');
-// let son3 = prompt('3ch  sonni kiriting: ');
+// let son1 = prompt("Birinchi sonni kiriting:");
+// let son2 = prompt("Ikkinchi sonni kiriting:");
+// let son3 = prompt("Uchinchi sonni kiriting:");
 
-// let bosh = 0;
-// son1 = parseInt(son1);
+// son1 = Number(son1); 
+// son2 = Number(son2);
+// son3 = Number(son3);
 
-// son2 = parseInt(son2);
-// son3 = parseInt(son3);
+// let kichik, orta, katta;  
+
+// if (son1 <= son2 && son1 <= son3) {
+//   kichik = son1;
+//   if (son2 <= son3) {
+//     orta = son2;
+//     katta = son3;
+//   } else {
+//     orta = son3;
+//     katta = son2;
+//   }
+// } else if (son2 <= son1 && son2 <= son3) {
+//   kichik = son2;
+//   if (son1 <= son3) {
+//     orta = son1;
+//     katta = son3;
+//   } else {
+//     orta = son3;
+//     katta = son1;
+//   }
+// } else {
+//   kichik = son3;
+//   if (son1 <= son2) {
+//     orta = son1;
+//     katta = son2;
+//   } else {
+//     orta = son2;
+//     katta = son1;
+//   }
+// }
+// console.log("Tartiblangan sonlar: " + kichik + ", " + orta + ", " + katta);
 
 
-// if (son1 > 0) bosh++;
-// if (son2 > 0) bosh++;
-// if (son3 > 0) bosh++;
-// console.log("Musbat sonlar:" + bosh);
+
+// 4-misol
+// let son1 = prompt("Birinchi sonni kiriting:");
+// let son2 = prompt("Ikkinchi sonni kiriting:");
+// let son3 = prompt("Uchinchi sonni kiriting:");
+
+// if (son1 === son2 && son1 !== son3) {
+//   console.log("Teng bo'lmagan son uchinchisi (son3). Tartib raqami: 3");
+// } else if (son1 === son3 && son1 !== son2) {
+//   console.log("Teng bo'lmagan son ikkinchisi (son2). Tartib raqami: 2");
+// } else if (son2 === son3 && son2 !== son1) {
+//   console.log("Teng bo'lmagan son birinchisi (son1). Tartib raqami: 1");
+// } else {
+//   console.log("Hech qaysi ikkitasi teng emas yoki uchalasi ham teng.");
+// }
+
+
+// 5-misol
+// let x = prompt("x sonini kiriting:");
+// x = Number(x);
+// let y; 
+// if (x < -2 || x > 2) {
+//   y = 2 * x;
+// } else {
+//   y = -3 * x; 
+// }
+// console.log("Funksiya qiymati: " + y);
+
+
+// 6-misol
+
 
 
